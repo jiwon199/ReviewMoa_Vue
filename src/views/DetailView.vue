bf6393255b935999bba4a45b06821f7d5d4eb142
jiwon <jiwonchoi99@naver.com>
2022-12-09 20:27:55 +0900

    라우터 페이지  추가

---
+++ src/views/DetailView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+     여기는 디테일 페이지입니다.
+  </div>
+</template>
+
+<script>
+ 
+export default {
+  name: 'DetailView',
+   
+}
+</script>
---
+++ src/views/UpdateView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+     여기는 리뷰 수정 페이지입니다.
+  </div>
+</template>
+
+<script>
+ 
+export default {
+  name: 'UpdateView',
+   
+}
+</script>
