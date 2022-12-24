782785fc9844aa7643fe23c381a7280961c0260e
lj05117 <lj05117@naver.com>
2022-12-24 23:44:30 +0900

    fix: complete not used error

--- src/store/modules/memberStore.js
+++ src/store/modules/memberStore.js
@@ -1,5 +1,6 @@
 
 const memberStore = {
+    namespaced: true,
     state: {
         isLogin: false,
         isLoginError: false,
@@ -13,7 +14,48 @@ const memberStore = {
     mutations: {
         SET_IS_LOGIN: (state, isLogin) => {
             state.isLogin = isLogin;
-        }
-        //이어서 적기
-    }
+        },
+        SET_IS_LOGIN_ERROR: (state, isLoginError) => {
+            state.isLoginError = isLoginError;
+        },
+        SET_USER_INFO: (state, userInfo) => {
+            state.isLogin = true;
+            state.userInfo = userInfo;
+        },
+    },
+    // actions: {
+    //     async userConfirm({ commit }, user) {
+    //         await login(
+    //             user,
+    //             ({ data }) => {
+    //                 if (data.message === "success") {
+    //                     commit("SET_IS_LOGIN", true);
+    //                     commit("SET_IS_LOGIN_ERROR", false);
+    //                     commit("SET_USER_INFO", data.userInfo);
+    //                 } else {
+    //                     commit("SET_IS_LOGIN", false);
+    //                     commit("SET_IS_LOGIN_ERROR", true);
+    //                 }
+    //             },
+    //             (error) => {
+    //                 console.log(error);
+    //             }
+    //         );
+    //     },
+    //     async userLogout({ commit }, userid) {
+    //         await logout(
+    //             userid,
+    //             ({ data }) => {
+    //                 if (data.message === "success") {
+    //                     commit("SET_IS_LOGIN", false);
+    //                     commit("SET_USER_INFO", null);
+    //                 }
+    //             },
+    //             (error) => {
+    //                 console.log(error);
+    //             }
+    //         )
+    //     }
+    // }
 }
+export default memberStore;
