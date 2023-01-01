1ccf3a45fb5f56a3882cd9308e1449f312c71b8d
jiwon <jiwonchoi99@naver.com>
2023-01-01 20:49:37 +0900

    디자인 수정

--- src/components/HeaderNavBar.vue
+++ src/components/HeaderNavBar.vue
@@ -1,19 +1,42 @@
 <template>
   <nav class="topmenu">
     <ul class="nav">
-      <li><router-link to="/" class="link" id="logo">리뷰 모아</router-link></li>
+      <li>
+        <router-link to="/" class="link" id="logo">리뷰 모아</router-link>
+      </li>
       <li><router-link to="/" class="link">리뷰 보기</router-link></li>
       <li><router-link to="/write" class="link">리뷰 쓰기</router-link></li>
-      <div v-if="userInfo">
-        <li style="display:inline-block">{{userInfo.realId}}님 환영합니다.</li>
-        <li style="display:inline-block" @click.prevent="onClickLogout" class="link">로그아웃 </li>
-        <li style="display:inline-block" @click.prevent="onClickDelete" class="link"> 회원 탈퇴</li>         
+      <div v-if="userInfo" style="display: inline-block">
+        
+        <li
+          style="display: inline-block"
+          @click.prevent="onClickLogout"
+          class="link"
+        >
+          로그아웃
+        </li>
+        <li
+          style="display: inline-block"
+          @click.prevent="onClickDelete"
+          class="link"
+        >
+          회원 탈퇴
+        </li>
+         
+         <li style="position: absolute; top:0px; right: 100px; color:white">
+          {{ userInfo.realId }}님 환영합니다.
+        </li>  
       </div>
       <div v-else>
-        <li style="display:inline-block"><router-link :to="{name:'regist'}" class="link">회원 가입</router-link></li> 
-        <li style="display:inline-block"><router-link :to="{name:'login'}" class="link">로그인</router-link></li>
+        <li style="display: inline-block">
+          <router-link :to="{ name: 'regist' }" class="link"
+            >회원 가입</router-link
+          >
+        </li>
+        <li style="display: inline-block">
+          <router-link :to="{ name: 'login' }" class="link">로그인</router-link>
+        </li>
       </div>
-      
     </ul>
   </nav>
 </template>
@@ -39,7 +62,7 @@ export default {
       console.log("환영합니다 ",this.userInfo.realId);
       // console.log("바로 읽기",this.$memberStore.state.userInfo.realId);
     },
-    
+
   },
   methods:{
     ...mapActions(memberStore,["userLogout"]),
@@ -51,11 +74,10 @@ export default {
 			this.SET_USER_INFO("");
 
       // this.userLogout(this.userInfo.realId);
-      if(this.$route.name!="home") 
+      if(this.$route.name!="home")
       this.$router.push({name:"home"});
     },
     onClickDelete(){
-onClickDelete(){
       console.log(this.userInfo.userId);
       const id = this.userInfo.userId;
       axios({
@@ -65,13 +87,6 @@ onClickDelete(){
         console.log("회원탈퇴 완료");
          this.SET_IS_LOGIN(false);
 				this.SET_USER_INFO("");
-
-      // this.userLogout(this.userInfo.realId);
-      if(this.$route.name!="home") 
-      this.$router.push({name:"home"});
-      })
-     
-    }
 
       // this.userLogout(this.userInfo.realId);
       if(this.$route.name!="home") 
@@ -94,6 +109,7 @@ onClickDelete(){
   list-style: none;
   line-height: 7vh;
   text-align: center;
+   
 }
 .link {
   color: PeachPuff;
@@ -101,10 +117,10 @@ onClickDelete(){
   width: 100px;
   display: block;
 }
-#logo{
+#logo {
   color: white;
   font-weight: bold;
-   
+
   width: 200px;
   display: block;
 }
